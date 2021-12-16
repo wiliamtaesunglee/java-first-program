55bd367f327e0e4cc30ef1be250f2a4205e7bc63
wiliam lee <wiliam.tae@gmail.com>
2021-12-16 06:02:40 -0300

    Complate module

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -56,6 +56,6 @@ public class SavingsCalculator {
 
         final SavingsCalculator calculator = new SavingsCalculator(credits, debits);
         float netSavings = calculator.calculate();
-        System.out.println("Net Savings =" + netSavings + ", remaining days in month = " + remainingDaysInMonth(LocalDate.now()));
+        System.out.println("Net Savings = " + netSavings + ", remaining days in month = " + remainingDaysInMonth(LocalDate.now()));
     }
 }
