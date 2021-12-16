e7ea32bcdbe9bd648150fc8f1dc24d7f40b9c5a3
wiliam lee <wiliam.tae@gmail.com>
2021-12-15 21:31:58 -0300

    Completed module

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,41 @@
+package com.h2;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits() {
+        float sum = 0.0f;
+        for(float i : credits) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits() {
+        float sum = 0.0f;
+        for(float i : debits) {
+             sum += i;
+        }
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date) {
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = date.getDayOfMonth() - totalDaysInMonth;
+        return remainingDays;
+    }
+
+    public float calculate() {
+        return 0;
+    }
+}
