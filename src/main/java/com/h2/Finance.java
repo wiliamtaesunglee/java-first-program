5bed2e92693e1fb3e4fa590d26521e40bb964d1f
wiliam lee <wiliam.tae@gmail.com>
2021-12-16 10:28:53 -0300

    Conclude module 4

---
+++ src/main/java/com/h2/Finance.java
@@ -0,0 +1,27 @@
+package com.h2;
+
+import java.util.Map;
+
+public class Finance {
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
+    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
+    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+    public static Map<String, String> commandsToUsage = Map.of(
+            BEST_LOAN_RATES, "usage: bestLoanRates",
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
+    );
+
+    private static boolean validateCommandArguments(String[] args) {
+        switch (args[0]) {
+            case BEST_LOAN_RATES:
+                return args.length == 1;
+            case SAVINGS_CALCULATOR:
+                return args.length == 3;
+            case MORTGAGE_CALCULATOR:
+                return args.length == 4;
+        }
+        return false;
+    }
+}
